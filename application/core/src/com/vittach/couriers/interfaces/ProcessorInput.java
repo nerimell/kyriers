d103fc21cdec8a3d28bf6d1e2231a4783db9e2f0
Xbox <zharikov.vitalik@yandex.ru>
2016-09-12 14:54:23 +0300

    second commit

--- application/core/src/com/vittach/couriers/interfaces/ProcessorInput.java
+++
@@ -1,7 +0,0 @@
-package com.vittach.couriers.interfaces;
-import com.badlogic.gdx.InputProcessor;
-
-public interface ProcessorInput
-    extends InputProcessor {
-    void setIDOffset(int offset);
-}
